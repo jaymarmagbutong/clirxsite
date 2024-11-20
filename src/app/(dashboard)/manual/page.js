25d95e2886a360f773c4334a68b88ebc6749d6e1
Jaymar Magbutong <magbutongjaymar@gmail.com>
2024-11-20 11:47:19 +0800

    Updates - 20-11-24
    Updates - 20-11-24

--- src/app/(dashboard)/manual/page.js
+++ src/app/(dashboard)/manual/page.js
@@ -1,27 +1,6 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import { useSocket } from '@/app/context/SocketContext';
+import React from 'react'
 
 const page = () => {
-
-	const socket = useSocket();
-
-	useEffect(() => {
-        if (!socket) return;
-    
-        // Set up the message listener
-        socket.on('message', (message) => {
-          console.log('Message from serverdds:', message);
-          // Here, you could update notifications or handle any other real-time updates
-        });
-    
-        // Clean up the listener on component unmount
-        return () => {
-          socket.off('message');
-        };
-      }, [socket]);
-
-
   return (
     <div>
         Manual Page
